6b2bbbc011de689e76e2949e3c881d8f83a28632
marouan ait tigargant <forbiamarouane@gmail.com>
2020-12-09 18:38:18 +0100

    Add Models & Tables migration for Product and Category

---
+++ resources/views/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Task Laravel with VueJs</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <script src="{!! mix('js/app.js') !!}" defer></script>
+        <link href="{!! mix('css/app.css') !!}" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css"></script>
+    </head>
+    <body>
+        <div id="app">
+            <div class="container">
+                <div class="row">
+                    <nav class="navbar navbar-expand-lg col-12 bg-primary my-3">
+                    </nav>
+                    test
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
